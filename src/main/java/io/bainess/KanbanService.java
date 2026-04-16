2fc02e90a74408cb94bc9758cfacec68098310f7
Inessa Babenko <iness.tt.eln@gmail.com>
2026-04-16 21:46:46 +0700

    feat: start the project pom / dockerfile / compose.yaml / checkstyle

---
+++ src/main/java/io/bainess/KanbanService.java
@@ -0,0 +1,11 @@
+package io.bainess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanService {
+    public static void main(String[] args) {
+        SpringApplication.run(KanbanService.class, args);
+    }
+}
